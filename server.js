35101e4a72f5bfe660fa79343349ad71ce3409fe
Mohammad <its_mh@outlook.com>
2024-02-01 11:09:44 +0300

    getting ready to deploy backend only

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/posts", require("./routes/api/posts"));
 
 // Serve static assets in production
+/*
 if (process.env.NODE_ENV === "production") {
 	// Set static folder
 	app.use(express.static("client/build"));
@@ -28,6 +29,7 @@ if (process.env.NODE_ENV === "production") {
 		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 	});
 }
+*/
 
 const PORT = process.env.PORT || 5000;
 
